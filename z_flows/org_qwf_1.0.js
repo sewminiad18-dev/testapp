886eb3455168c91ad558863555f6ac5d9ee1f5d5
sewmini@linearsix.com <sewmini@linearsix.com>
2025-11-20 21:46:54 +0530

    files added SL-1
    Signed-off-by: sewmini@linearsix.com <sewmini@linearsix.com>

---
+++ z_flows/org_qwf_1.0.js
@@ -0,0 +1,20 @@
+this = {
+    "name": "Inputter",
+    "version": "1.0",
+    "stages": [
+        {
+            "name": "stage1",
+            "function": function (CONTEXT) {
+                let a = 12
+            },
+        },
+        {
+            "name": "stage2",
+            "function": function (CONTEXT) {
+               //This is this. 9.46 inpuuter change
+               let x = 12 + 5
+               let y = x *100;
+            },
+        }
+    ]
+}
---
+++ z_flows/org_qwf_appr_1.0.js
@@ -0,0 +1,20 @@
+this = {
+    "name": "Approver",
+    "version": "1.0",
+    "stages": [
+        {
+            "name": "stage1",
+            "function": function (CONTEXT) {
+                //CONTEXT.next_stage = ["branch", "Sampath_ORG_CUST_B3_Quick_customer_onboarding_summary:2.0", "start"];
+            },
+        },
+        {
+            "name": "stage2",
+            "function": function (CONTEXT) {
+               //This is this. 9.45 change
+               let x = 12 + 5
+               let y = x *100;
+            },
+        }
+    ]
+}
